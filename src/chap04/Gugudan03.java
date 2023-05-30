d700ce907faa02dccc5c12c5b51d5de863731805
JiWonKim <hghg1036@naver.com>
2023-05-30 14:50:21 +0900

    구구단 레벨3 구현완료

--- src/chap04/Gugudan03.java
+++ src/chap04/Gugudan03.java
@@ -5,19 +5,27 @@ package chap04;
  단, 하나의 for 반복문을 사용합니다.
  
  실행 예]
-
+ [ 2 단 ]
  2 * 1 =  2
  2 * 2 =  4
  2 * 3 =  6
  . . .
+ 2 * 9 = 18
+ 
+  [ 3 단 ]
  3 * 1 =  3
+ . . .
+ 4 * 9 = 36
 */
 
-public class Gugudan02 {
+public class Gugudan03 {
 
 	public static void main(String[] args) {
 		int dan = 2, num = 1;
 		for (int i = 0; i < 72; i++) {// 횟수만 제어
+			if (num == 1) {
+				System.out.printf("\n [ %d 단 ]\n", dan);
+			}
 			System.out.printf("%d * %d = %2d\n", dan, num, dan);
 			num++;
 			if (num == 10) {
