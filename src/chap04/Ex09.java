dc8775d8bc799a017f7a545fd04a248e4317d55c
JiWonKim <hghg1036@naver.com>
2023-05-30 17:45:07 +0900

    while을 do-while문으로 변경 공부완료

---
+++ src/chap04/Ex09.java
@@ -0,0 +1,20 @@
+package chap04;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		int total = 0;
+
+//		for (int i = 1; i <= 100; i++) {
+//			total += i;
+//		}
+//		System.out.println("1 부터 100까지의 합: " + total);
+
+		int i = 1; // 1. 초기식을 뺀다.
+		do {// 3. 조건만 남게 된다.
+			total += i;
+			i++;// 2. 증감식을 마지막 실행으로 뺀다.
+		} while (i <= 100);
+		System.out.println("1 부터 100까지의 합: " + total);
+	}
+}
