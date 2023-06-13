a4d2e9a68222ecd6f10f709ff497a37e405e0c96
JiWonKim <hghg1036@naver.com>
2023-06-13 12:38:24 +0900

    final static 예제 추가완료

--- src/chap07/Circle.java
+++ src/chap07/Circle.java
@@ -2,15 +2,21 @@ package chap07;
 
 public class Circle {
 
-	static final double PI = 3.141592;
-	final int num;
+	static final double PI = 3.141592; // static final 변수는 반드시 선언과 동시에 초기화 해주어야 한다.
+	final int radius; // 선언과 초기화 분리가 가능하다.
 
 	static {
 //		Circle.PI = 3.141592;
 	}
 
-	Circle() {
-		num = 1234;
+	Circle(int radius) {
+		this.radius = 1234;
+	}
+
+	double getArea() {
+		final double result;
+		result = this.radius * this.radius * Circle.PI;
+		return result;
 	}
 
 }
---
+++ src/chap07/Ex5.java
@@ -0,0 +1,14 @@
+package chap07;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		Circle circle = new Circle(7);
+
+		System.out.println("PI: " + Circle.PI);
+//		circle.PI = 3.14;
+		
+		System.out.println("radius: " + circle.radius);
+//		circle.radius= 7;
+	}
+
+}
