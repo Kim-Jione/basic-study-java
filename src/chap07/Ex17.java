0c2611b889d53ed69948a3d518d1491d5984ac65
JiWonKim <hghg1036@naver.com>
2023-06-14 15:13:09 +0900

    다형성의 특징 공부완료

--- src/chap07/Ex17.java
+++ src/chap07/Ex17.java
@@ -11,18 +11,17 @@ public class Ex17 {
 		FireEngine fire = new FireEngine(); // 소방차를 차로 바꿨다가 다시 소방차로 바꾼 형식
 		Ambulance ambulance = new Ambulance();
 
-		System.out.println("c1 instanceof Car: " + (myCar instanceof Car));
-		System.out.println("c1 instanceof FireEngine: " + (myCar instanceof FireEngine));
-		System.out.println("c1 instanceof ambulance: " + (myCar instanceof Ambulance));
+		System.out.println("c1 instanceof Car: " + (c1 instanceof Car));
+		System.out.println("c1 instanceof FireEngine: " + (c1 instanceof FireEngine));
+		System.out.println("c1 instanceof ambulance: " + (c1 instanceof Ambulance));
 
-		System.out.println("c2 instanceof Car: " + (fire instanceof Car));
-		System.out.println("c2 instanceof FireEngine: " + (fire instanceof FireEngine));
-//		System.out.println("c2 instanceof ambulance: " + (fire instanceof Ambulance));
+		System.out.println("c2 instanceof Car: " + (c2 instanceof Car));
+		System.out.println("c2 instanceof FireEngine: " + (c2 instanceof FireEngine));
+		System.out.println("c2 instanceof ambulance: " + (c2 instanceof Ambulance));
 
-		System.out.println("c3 instanceof Car: " + (ambulance instanceof Car));
-//		System.out.println("c3 instanceof FireEngine: " + (ambulance instanceof FireEngine));
-		System.out.println("c3 instanceof ambulance: " + (ambulance instanceof Ambulance));
+		System.out.println("c3 instanceof Car: " + (c3 instanceof Car));
+		System.out.println("c3 instanceof FireEngine: " + (c3 instanceof FireEngine));
+		System.out.println("c3 instanceof ambulance: " + (c3 instanceof Ambulance));
 
-	
 	}
 }
---
+++ src/chap07/Ex18.java
@@ -0,0 +1,29 @@
+package chap07;
+
+public class Ex18 {
+
+	public static void main(String[] args) {
+
+		Car[] cars = new Car[3];
+		cars[0] = new Car();
+		cars[1] = new FireEngine();
+		cars[2] = new Ambulance();
+
+		for (Car myCar : cars) {
+			myCar.drive();
+			// 소방차이면 물을 뿌리고 구급차이면 사이렌을 울리는 코드를 작성하세요.
+			if (myCar instanceof FireEngine) {
+				FireEngine fire = (FireEngine) myCar;
+				fire.water();
+			}
+			if (myCar instanceof Ambulance) {
+				Ambulance ambulance = (Ambulance) myCar;
+				ambulance.siren();
+			}
+
+			myCar.stop();
+
+		}
+
+	}
+}
